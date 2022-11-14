2ae1b1cf97af31e1b2b91fcab5d6911a22bfbd71
Tanakaaaaaa <carboots11@gmail.com>
2022-11-14 16:23:23 +0900

    ルート追記、マイグレーションファイル作成

---
+++ database/migrations/2022_11_14_150528_create_todolists_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id', 10)->unsigned();
+            $table->string('name', 40);
+            $table->string('text', 255);
+            $table->integer('status')->default(1);
+            $table->date('limit_date');
+            $table->date('completion_date');
+            $table->timestamps();
+            $table->softDeletes();
+
+            // 外部キーを設定する
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolists');
+    }
+};
---
+++ database/migrations/2022_11_14_160847_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('login_id', 10);
+            $table->string('password', 10);
+            $table->string('name', 100);
+            $table->string('furigana', 100);
+            $table->string('gender', 10);
+            $table->date('birthday');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
